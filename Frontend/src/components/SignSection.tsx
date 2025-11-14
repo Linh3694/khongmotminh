1ba5b15299e7c1041ac170073dea81e6c54499d8
Linh3694 <hailinh.n23@gmail.com>
2025-11-14 09:43:19 +0700

    update

--- Frontend/src/components/SignSection.tsx
+++ Frontend/src/components/SignSection.tsx
@@ -162,7 +162,6 @@ const SignSection = () => {
                     name="currentPosition"
                     type="text"
                     className="w-full px-3 py-2 border border-black focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    required
                   />
                 </div>
               </div>
